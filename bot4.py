2f208d870fe64f1b74d7cf8b92aa099631f1fd58
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-08 16:14:51 +0200

    Update bot4.py

--- bot4.py
+++ bot4.py
@@ -4,10 +4,14 @@ import requests
 TOKEN = os.environ.get("TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
+print("TOKEN:", TOKEN)
+print("CHAT_ID:", CHAT_ID)
+
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+    r = requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+    print("STATUS:", r.status_code)
+    print("RESPONSE:", r.text)
 
 if __name__ == "__main__":
-    send_message("🚀 BOT GITHUB TEST OK")
-    print("done")
+    send_message("TEST DEBUG BOT")
